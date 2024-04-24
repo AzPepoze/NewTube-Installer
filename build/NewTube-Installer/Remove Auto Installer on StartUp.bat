754a70da886ba044c2e2abf43c00bdc0377a9e3f
AzPepoze <65204787+AzPepoze@users.noreply.github.com>
2024-04-24 18:54:47 +0700

    chore: update .gitignore

---
+++ build/NewTube-Installer/Remove Auto Installer on StartUp.bat
@@ -0,0 +1,14 @@
+@echo off
+set "shortcutName=Newtube-Installer"
+set "startupFolder=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
+
+if exist "%startupFolder%\%shortcutName%.lnk" (
+    echo Removing shortcut...
+    del "%startupFolder%\%shortcutName%.lnk"
+    echo Removed.
+) else (
+    echo Not found "%shortcutName%" in Startup Folder.
+)
+
+echo.
+pause
